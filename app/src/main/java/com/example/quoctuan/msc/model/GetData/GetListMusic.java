ba751459817f3105f9544f1e44efbe6f1312ea0c
tung.nguyenvan <nguyentungtungtungnguyen@gmail.com>
2018-04-30 14:31:46 +0700

    update class getlistmusic

--- app/src/main/java/com/example/quoctuan/msc/model/GetData/GetListMusic.java
+++ app/src/main/java/com/example/quoctuan/msc/model/GetData/GetListMusic.java
@@ -19,19 +19,23 @@ import java.util.List;
 public class GetListMusic {
     private List<Songs> arrayList;
     private Activity activity;
+    private Cursor cursor;
 
     public GetListMusic(Activity activity){
         this.activity = activity;
-    }
-
-    public List<Songs> GetListMusic(){
-        arrayList = new ArrayList<>();
         ContentResolver contentResolver = activity.getContentResolver();
         Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         String selection = MediaStore.Audio.Media.IS_MUSIC + " !=0";
         String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
-        Cursor cursor = contentResolver.query(uri, null, selection, null , sortOrder);
+        cursor = contentResolver.query(uri, null, selection, null , sortOrder);
+    }
 
+    public int CountMusic(){
+        return cursor.getColumnCount();
+    }
+
+    public List<Songs> GetListMusic(){
+        arrayList = new ArrayList<>();
         if (cursor != null){
             int count = cursor.getCount();
             Log.d("count", count + "");
@@ -48,4 +52,18 @@ public class GetListMusic {
 
         return arrayList;
     }
+
+    public List<String> GetListAlbum(){
+        List<String> arrayListAlbum = new ArrayList<>();
+        if (cursor != null){
+            int count = cursor.getCount();
+            while (count > 0){
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                arrayListAlbum.add(album);
+            }
+        }
+
+        return arrayListAlbum;
+    }
+
 }
